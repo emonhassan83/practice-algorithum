436f23bdfacd552fa77a64dfa519c8fee4c79046
Emon <emonhasan7650@gmail.com>
2024-09-03 20:46:07 +0600

    concept seesion 1 code added done

---
+++ week-01.Basic_Graph_BFS_&_DFS/concept-session-01/1_vectorOfvector.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    // type-1
+    vector<vector<int>> arr1;
+
+    arr1.push_back(vector<int>());
+    arr1.push_back(vector<int>());
+    // arr1.push_back(vector<int>({1,2,3}));
+    // arr1.push_back(vector<int>({4,5,6}));
+
+    // arr1[0].push_back(7);
+
+    cout << arr1.size() << endl;
+    // cout<<arr1[0].size()<<endl;
+
+    // for(int i=0;i<arr1.size();i++){
+    //     for(int j=0;j<arr1[i].size();j++){
+    //         cout<<arr1[i][j]<<" ";
+    //     }
+    //     cout<<endl;
+
+    // }
+
+    // type-2
+    vector<vector<int>> arr2;
+
+    return 0;
+}
