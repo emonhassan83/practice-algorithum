0d54039e5f0a87e61e3d8f1409ff446472acde4b
Emon <emonhasan7650@gmail.com>
2024-09-11 23:16:56 +0600

    dijkstra algorithum related problem solved

--- week-02. Graph Algorithum/2.5. week-02_practice_01/3. dijkstra_algorithm.cpp
+++ week-02. Graph Algorithum/2.5. week-02_practice_01/3. dijkstra_algorithm.cpp
@@ -1,2 +1,67 @@
 //* https://www.geeksforgeeks.org/problems/detect-cycle-in-a-directed-graph/1?utm_source=geeksforgeeks&utm_medium=ml_article_practice_tab&utm_campaign=article_practice_tab
 
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    bool dfs(int node, vector<int> adj[], vector<bool> &visited, vector<bool> &pathVisited)
+    {
+        visited[node] = true;
+        pathVisited[node] = true;
+
+        for (int neighbor : adj[node])
+        {
+            if (!visited[neighbor])
+            {
+                if (dfs(neighbor, adj, visited, pathVisited))
+                    return true;
+            }
+            else if (pathVisited[neighbor])
+                return true; //* Cycle found
+        }
+
+        pathVisited[node] = false; //* Backtracking
+        return false;
+    }
+
+    //* Function to detect cycle in a directed graph.
+    bool isCycle(int V, vector<int> adj[])
+    {
+        vector<bool> visited(V, false);
+        vector<bool> pathVisited(V, false);
+
+        for (int i = 0; i < V; i++)
+        {
+            if (!visited[i])
+            {
+                if (dfs(i, adj, visited, pathVisited))
+                    return true;
+            }
+        }
+        return false;
+    }
+};
+
+int main()
+{
+    int V, E;
+    cin >> V >> E;
+    vector<int> adj[V];
+
+    for (int i = 0; i < E; i++)
+    {
+        int u, v;
+        cin >> u >> v;
+        adj[u].push_back(v);
+    }
+
+    Solution obj;
+    if (obj.isCycle(V, adj))
+        cout << "1\n";
+    else
+        cout << "0\n";
+
+    return 0;
+}
--- week-02. Graph Algorithum/2.5. week-02_practice_01/4. dijkstra_algorithum2.cpp
+++ week-02. Graph Algorithum/2.5. week-02_practice_01/4. dijkstra_algorithum2.cpp
@@ -1,1 +1,82 @@
 //* https://basecamp.eolymp.com/en/problems/1365
+
+#include <bits/stdc++.h>
+using namespace std;
+
+const int N = 105;
+const int INF = INT_MAX;
+
+vector<pair<int, int>> adj[N];
+int dist[N];
+
+//* Dijkstra's algorithm using a priority queue
+void dijkstra(int src, int n)
+{
+    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+
+    for (int i = 0; i <= n; ++i)
+        dist[i] = INF;
+        
+    dist[src] = 0;
+
+    pq.push({0, src});
+
+    while (!pq.empty())
+    {
+        int u = pq.top().second;
+        int currDist = pq.top().first;
+        pq.pop();
+
+        //* Skip processing if we have already found a better distance
+        if (currDist > dist[u])
+            continue;
+
+        //* Relaxation step for all neighbors of u
+        for (auto edge : adj[u])
+        {
+            int v = edge.first;
+            int weight = edge.second;
+
+            if (dist[u] + weight < dist[v])
+            {
+                dist[v] = dist[u] + weight;
+                pq.push({dist[v], v});
+            }
+        }
+    }
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    int n, s, f;
+    cin >> n >> s >> f;
+
+    s--;
+    f--;
+
+    //* Reading the adjacency matrix
+    for (int i = 0; i < n; ++i)
+    {
+        for (int j = 0; j < n; ++j)
+        {
+            int weight;
+            cin >> weight;
+
+            if (weight != -1 && i != j)
+                adj[i].push_back({j, weight});
+        }
+    }
+
+    dijkstra(s, n);
+
+    if (dist[f] == INF)
+        cout << -1 << endl; //* No path found
+    else
+        cout << dist[f] << endl; //* Shortest distance found
+
+    return 0;
+}
