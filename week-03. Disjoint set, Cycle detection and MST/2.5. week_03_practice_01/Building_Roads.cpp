991b07efc128cdd962d20c1abbc4977c968e186e
Emon <emonhasan7650@gmail.com>
2024-09-21 15:07:47 +0600

    week 03 practice day 1 solved problem done

--- week-03. Disjoint set, Cycle detection and MST/2.5. week_03_practice_01/Building_Roads.cpp
+++ week-03. Disjoint set, Cycle detection and MST/2.5. week_03_practice_01/Building_Roads.cpp
@@ -1,7 +1,81 @@
+//* https://cses.fi/problemset/task/1666
+
 #include <bits/stdc++.h>
 using namespace std;
+
+const int N = 1e5 + 5;
+int par[N];
+int group_size[N];
+
+void dsu_initialize(int n)
+{
+    for (int i = 1; i <= n; i++)
+    {
+        par[i] = -1;
+        group_size[i] = 1; //* for size track
+    }
+}
+
+int dsu_find(int node)
+{
+    if (par[node] == -1)
+        return node;
+
+    int leader = dsu_find(par[node]);
+    par[node] = leader;
+
+    return leader;
+}
+
+void dsu_union_by_size(int node1, int node2)
+{
+    int leaderA = dsu_find(node1);
+    int leaderB = dsu_find(node2);
+
+    if (leaderA == leaderB)
+        return; //* if find same leader then return
+
+    //* compromise leader by group size
+    if (group_size[leaderA] > group_size[leaderB])
+    {
+        par[leaderB] = leaderA;
+        group_size[leaderA] += group_size[leaderB]; //* update or marge group size
+    }
+    else
+    {
+        par[leaderA] = leaderB;
+        group_size[leaderB] += group_size[leaderA]; //* update or marge group size
+    }
+}
+
 int main()
 {
-    
+    int n, m;
+    cin >> n >> m;
+
+    dsu_initialize(n);
+
+    while (m--)
+    {
+        int a, b;
+        cin >> a >> b;
+
+        dsu_union_by_size(a, b);
+    }
+
+    vector<int> leaders;
+    for (int i = 1; i <= n; i++)
+    {
+        if (par[i] == -1)
+            leaders.push_back(i);
+    }
+
+    cout << leaders.size() - 1 << endl;
+
+    for (int i = 1; i <= leaders.size() - 1; i++)
+    {
+        cout << leaders[i - 1] << " " << leaders[i] << endl;
+    }
+
     return 0;
 }
--- week-03. Disjoint set, Cycle detection and MST/2.5. week_03_practice_01/Message_Route.cpp
+++ week-03. Disjoint set, Cycle detection and MST/2.5. week_03_practice_01/Message_Route.cpp
@@ -1,7 +1,82 @@
 #include <bits/stdc++.h>
 using namespace std;
+
+int n, m;
+const int N = 1e5 + 5;
+vector<int> adj[N];
+int parent[N];
+bool visited[N];
+
+vector<int> bfs(int src, int des)
+{
+    queue<int> q;
+    q.push(src);
+
+    visited[src] = true;
+    parent[src] = -1;
+
+    while (!q.empty())
+    {
+        int p = q.front();
+        q.pop();
+
+        for (int child : adj[p])
+        {
+            if (!visited[child])
+            {
+                visited[child] = true;
+                parent[child] = p;
+                q.push(child);
+
+                //* if we reach the destination
+                if (child == des)
+                {
+                    vector<int> path;
+                    int current = des;
+
+                    while (current != -1)
+                    {
+                        path.push_back(current);
+                        current = parent[current];
+                    }
+
+                    reverse(path.begin(), path.end());
+                    return path;
+                }
+            }
+        }
+    }
+
+    return {};
+}
+
 int main()
 {
-    
+    cin >> n >> m;
+
+    for (int i = 0; i < m; i++)
+    {
+        int a, b;
+        cin >> a >> b;
+
+        adj[a].push_back(b);
+        adj[b].push_back(a); //* it's an undirected graph
+    }
+
+    //* Perform BFS to find the shortest path from 1 to n
+    vector<int> path = bfs(1, n);
+
+    if (path.empty())
+        cout << "IMPOSSIBLE" << endl;
+    else
+    {
+        cout << path.size() << endl;
+
+        for (int node : path)
+            cout << node << " ";
+
+        cout << endl;
+    }
+
     return 0;
 }
