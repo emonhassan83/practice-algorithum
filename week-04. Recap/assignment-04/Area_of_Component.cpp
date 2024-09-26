901b0c602574ac0be0e2b181bc5f9f70b34c9dfc
Emon <emonhasan7650@gmail.com>
2024-09-26 23:57:24 +0600

    3 problem solved done in algorithum mst, dsu, and bfs related done

--- week-04. Recap/assignment-04/Area_of_Component.cpp
+++ week-04. Recap/assignment-04/Area_of_Component.cpp
@@ -1,7 +1,70 @@
 #include <bits/stdc++.h>
 using namespace std;
+
+const int N = 1e3 + 5;
+char grid[N][N];
+bool vis[N][N];
+int n, m;
+
+vector<pair<int, int>> d = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+
+bool valid(int ci, int cj)
+{
+    if (ci >= 0 && ci < n && cj >= 0 && cj < m)
+        return true;
+    else
+        return false;
+}
+
+int dfs(int si, int sj)
+{
+    vis[si][sj] = true;
+    int area = 1;
+
+    for (int i = 0; i < 4; i++)
+    {
+        int ci = si + d[i].first;
+        int cj = sj + d[i].second;
+
+        if (valid(ci, cj) && !vis[ci][cj] && grid[ci][cj] == '.')
+            area += dfs(ci, cj);
+    }
+    return area;
+}
+
 int main()
 {
-    
+    cin >> n >> m;
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+            cin >> grid[i][j];
+    }
+
+    memset(vis, 0, sizeof(vis));
+
+    int minArea = INT_MAX;
+    bool cmp = false;
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            if (grid[i][j] == '.' && !vis[i][j])
+            {
+                int area = dfs(i, j);
+
+                minArea = min(minArea, area);
+                cmp = true;
+            }
+        }
+    }
+
+    if (cmp)
+        cout << minArea << endl;
+    else
+        cout << -1 << endl;
+
     return 0;
 }
--- week-04. Recap/assignment-04/Cycle_of_Edges.cpp
+++ week-04. Recap/assignment-04/Cycle_of_Edges.cpp
@@ -1,7 +1,72 @@
 #include <bits/stdc++.h>
 using namespace std;
+
+const int N = 1e5 + 5;
+int par[N];
+int group_size[N];
+
+void dsu_initialize(int n)
+{
+    for (int i = 1; i <= n; i++)
+    {
+        par[i] = -1;
+        group_size[i] = 1;
+    }
+}
+
+int dsu_find(int node)
+{
+    if (par[node] == -1)
+        return node;
+
+    int leader = dsu_find(par[node]);
+    par[node] = leader;
+
+    return leader;
+}
+
+void dsu_union_by_size(int node1, int node2)
+{
+    int leaderA = dsu_find(node1);
+    int leaderB = dsu_find(node2);
+
+    if (group_size[leaderA] > group_size[leaderB])
+    {
+        par[leaderB] = leaderA;
+        group_size[leaderA] += group_size[leaderB];
+    }
+    else
+    {
+        par[leaderA] = leaderB;
+        group_size[leaderB] += group_size[leaderA];
+    }
+}
+
 int main()
 {
-    
+
+    int n, e;
+    cin >> n >> e;
+
+    dsu_initialize(n);
+
+    int cycleEdges = 0;
+
+    while (e--)
+    {
+        int u, v;
+        cin >> u >> v;
+
+        int leaderU = dsu_find(u);
+        int leaderV = dsu_find(v);
+
+        if (leaderU == leaderV)
+            cycleEdges++;
+        else
+            dsu_union_by_size(u, v);
+    }
+
+    cout << cycleEdges << endl;
+
     return 0;
 }
