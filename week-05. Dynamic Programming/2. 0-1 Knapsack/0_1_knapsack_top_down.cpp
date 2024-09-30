32523bfd31e23c4489e4ee03d27e2b7b6aebf955
Emon <emonhasan7650@gmail.com>
2024-09-30 23:54:10 +0600

    knapsack_top_down approch implement done

--- week-05. Dynamic Programming/2. 0-1 Knapsack/0_1_knapsack_top_down.cpp
+++ week-05. Dynamic Programming/2. 0-1 Knapsack/0_1_knapsack_top_down.cpp
@@ -1,10 +1,18 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+const int maxN = 1000;
+const int maxW = 1000;
+int dp[maxN][maxW];
+
 int knapsack(int n, int weight[], int value[], int W)
 {
     if (n < 0 || W == 0)
         return 0; //* base case
+
+    //* apply memoization condition
+    if(dp[n][W] != -1)
+        return dp[n][W];
 
     if (weight[n] <= W)
     {
@@ -13,13 +21,13 @@ int knapsack(int n, int weight[], int value[], int W)
         int option1 = knapsack(n - 1, weight, value, W - weight[n]) + value[n];
         int option2 = knapsack(n - 1, weight, value, W);
 
-        return max(option1, option2);
+        return dp[n][W] = max(option1, option2);
     }
     else
     {
         //* ekta option: na niye dekhbo
         int option2 = knapsack(n - 1, weight, value, W);
-        return option2;
+        return dp[n][W] = option2;
     }
 }
 
@@ -30,14 +38,20 @@ int main()
 
     int weight[n], value[n];
 
+    int W;
+    cin >> W;
+
     for (int i = 0; i < n; i++)
         cin >> weight[i];
 
     for (int i = 0; i < n; i++)
         cin >> value[i];
 
-    int W;
-    cin >> W;
+    for (int i = 0; i <= n; i++)
+    {
+        for (int j = 0; j <= W; j++)
+            dp[i][j] = -1;
+    }
 
     cout << knapsack(n, weight, value, W) << endl;
     return 0;
