115702dac074f04ee9b81057961cc413a1a7d914
Emon <emonhasan7650@gmail.com>
2024-10-04 11:20:31 +0600

    subset bottom up and count of subset sub(top down and bottom up) approch done

--- week-05. Dynamic Programming/3. 0_1 Kanpsack Variations/count_of_subset_sum.cpp
+++ week-05. Dynamic Programming/3. 0_1 Kanpsack Variations/count_of_subset_sum.cpp
@@ -2,15 +2,15 @@
 using namespace std;
 int dp[1005][1005];
 
-bool subset_sum(int n, int a[], int s)
+int subset_sum(int n, int a[], int s)
 {
     //* base case
     if (n == 0)
     {
         if (s == 0)
-            return true;
+            return 1;
         else
-            return false;
+            return 0;
     }
 
     //* for memoization
@@ -20,10 +20,10 @@ bool subset_sum(int n, int a[], int s)
     //* apply 0_1 knapsack and if we select or choses value in range
     if (a[n - 1] <= s)
     {
-        bool op1 = subset_sum(n - 1, a, s - a[n - 1]);
-        bool op2 = subset_sum(n - 1, a, s);
+        int op1 = subset_sum(n - 1, a, s - a[n - 1]);
+        int op2 = subset_sum(n - 1, a, s);
 
-        return dp[n][s] = op1 || op2;
+        return dp[n][s] = op1 + op2;
     }
     else
     {
@@ -43,17 +43,13 @@ int main()
     int s;
     cin >> s;
 
-    //* for initialize 
+    //* for initialize
     for (int i = 0; i <= n; i++)
     {
         for (int j = 0; j <= s; j++)
             dp[i][j] = -1;
     }
 
-    if (subset_sum(n, a, s))
-        cout << "YES" << endl;
-    else
-        cout << "NO" << endl;
-
+    cout << subset_sum(n, a, s) << endl; //* count of subset sum
     return 0;
 }
